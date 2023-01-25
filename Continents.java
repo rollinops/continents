0dd95ee8c1e6c471da7a8eead82c96e8a29c3a62
rollinops <gaiusom@yahoo.com>
2023-01-25 23:22:05 +0900

    version 3

--- Continents.java
+++ Continents.java
@@ -1,6 +1,6 @@
 public class Continents {
     public static void main(String[] args) {
-        String[] continents = {"Africa1", "Antarctica", "Asia", "Australia", "Europe", "North America", "South America"};
+        String[] continents = {"Africa", "Antarctica", "Asia", "Australia", "Europe", "North America", "South America"};
 
         for (String continent : continents) {
             System.out.println(continent);
